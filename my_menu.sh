2a0df4c966c38e4ed926e5cd067ea3ee91663103
venam <patrick@unixhub.net>
2013-07-26 01:29:10 +0300

    it was still there

--- my_menu.sh
+++ my_menu.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 9menu -label "Menu"\
       -bg "#222222" \
-      -fg "#d4c894"\
-      -font "-schumacher-clean-*-*-normal-*-10-*100-75-75-c-50-*-*"\
+      -fg "#7a88a3"\
+      -font "-*-pragmataprol-medium-*-*-*-10-*-100-100-*-*-*-*"\
       -popup\
       -teleport\
       "Urxvtc":"urxvtc --geometry 70x20"\
+      "Urxvtd":"urxvtd -q -o -f"\
       "Iconic":"hidden -c|xargs 9menu -popup -label Iconics -bg \"#222222\" -fg \"#d4c894\" -font \"-schumacher-clean-*-*-normal-*-10-*100-75-75-c-50-*-*\""\
-      "Lock":"i3lock -t -i /usr/share/backgrounds/Tile/mine/test_tile5.png -c 121212 -b"\
-      "Midori":"midori"\
+      "Gtk":"lxappearance"\
+      "Lock":"i3lock -t -i /usr/share/backgrounds/Tile/grayish/wild_oliva.png -c 121212 -b"\
+      "Icecat":"icecat"\
+      "xbrro":"xombrero"\
+      "Nightly":"firefox-nightly"\
+      "C-Blocks":"codeblocks"\
       "Mail":"sylpheed"\
       "Thunar":"thunar"\
