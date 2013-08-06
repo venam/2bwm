1b339f4d6a54557e6452f9a3e884c2baf42416aa
venam <patrick@unixhub.net>
2013-08-07 02:00:14 +0300

    the offsets needs to be updated, it doesn't work well in some cases

--- 2bwm.c
+++ 2bwm.c
@@ -1394,25 +1394,25 @@ void teleport(const Arg *arg)
     uint16_t mon_width, mon_height;
     getmonsize(&mon_x, &mon_y, &mon_width, &mon_height,focuswin);
     setignoreborder(&temp, focuswin,true);
-    focuswin->x = mon_x+offsets[0]+offsets[2];  focuswin->y = mon_y+offsets[1]+offsets[3];
+    focuswin->x = mon_x+offsets[0];  focuswin->y = mon_y;
 
     if (arg->i==0) {
-        focuswin->x  += mon_width - (focuswin->width + conf.borderwidth * 2) +mon_x;
-        focuswin->y  += mon_height - (focuswin->height + conf.borderwidth* 2)+ mon_y;
+        focuswin->x  += mon_width - (focuswin->width + conf.borderwidth * 2) +mon_x+offsets[0]+offsets[2];
+        focuswin->y  += mon_height - (focuswin->height + conf.borderwidth* 2)+ mon_y+offsets[1]+offsets[3];
         focuswin->y  = focuswin->y /2-(offsets[3]);
         focuswin->x  = focuswin->x /2-(offsets[2]);
     }
     else {
         if (arg->i>0) {
             if (arg->i<2)
-                focuswin->y += mon_height - (focuswin->height + conf.borderwidth* 2);
+                focuswin->y += mon_height - (focuswin->height + conf.borderwidth* 2)-offsets[3]+offsets[1];
         }
         else {
             if (arg->i<-1)
                 focuswin->x += mon_width - (focuswin->width + conf.borderwidth * 2);
             else {
                 focuswin->x += mon_width - (focuswin->width + conf.borderwidth * 2);
-                focuswin->y += mon_height - (focuswin->height + conf.borderwidth* 2);
+                focuswin->y += mon_height - (focuswin->height + conf.borderwidth* 2)-offsets[3]+offsets[1];
             }
         }
     }
