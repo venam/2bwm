8cc312eb0aeca07614c06b3f8f0aad3d575569ff
Maximus R.D. Parmer <maxp@pdx.edu>
2014-01-16 16:02:04 -0800

    Sizes wm_name_window buffer to reply_length+1 & ensures null termination.
    The output of xcb_get_property_value is not null-terminated, which may
    lead to unpredictable behavior of strstr (requires strings be null
    terminated, as provided by the string literal).
    
    Under Valgrind, this appears as:
    ==4260== Invalid read of size 1
    ==4260==    at 0x4A0C164: strstr (in /usr/lib64/valgrind/vgpreload_memcheck-amd64-linux.so)
    ==4260==    by 0x4054DF: setupwin (2bwm.c:334)
    ==4260==    by 0x402BD8: main (2bwm.c:706)
    ==4260==  Address 0x5464030 is 32 bytes inside a block of size 60 free'd
    ==4260==    at 0x4A07577: free (in /usr/lib64/valgrind/vgpreload_memcheck-amd64-linux.so)
    ==4260==    by 0x4054D2: setupwin (2bwm.c:332)
    ==4260==    by 0x402BD8: main (2bwm.c:706)

--- 2bwm.c
+++ 2bwm.c
@@ -326,7 +326,10 @@ void check_name(struct client *client)
         if (NULL!=reply) free(reply);
         return;
     }
-    char *wm_name_window = xcb_get_property_value(reply);
+    int reply_len = xcb_get_property_value_length(reply);
+    char *wm_name_window = malloc(sizeof(char) * (reply_len + 1));
+    memcpy(wm_name_window, xcb_get_property_value(reply), reply_len);
+    wm_name_window[reply_len] = '\0';
     if(NULL!=reply) free(reply);
     for(unsigned int i=0;i<sizeof(ignore_names)/sizeof(__typeof__(*ignore_names));i++)
         if (strstr(wm_name_window, ignore_names[i]) !=NULL) {
