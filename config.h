f31968b534bbe0b6dd13b83c4dd10d74cb194b48
Patrick Louis <patrick@iotek.org>
2017-03-28 20:44:33 +0300

    leave the setting of the commands to the user

--- config.h
+++ config.h
@@ -37,17 +37,7 @@ static const uint8_t borders[] = {3,5,5,4};
 #define LOOK_INTO "WM_NAME"
 static const char *ignore_names[] = {"bar", "xclock"};
 ///--Menus and Programs---///
-static const char *menucmd[]   = { "my_menu.sh", NULL };
-static const char *gmrun[]     = { "my_menu2.sh",NULL};
-static const char *terminal[]  = { "urxvtc", NULL };
-static const char *click1[]    = { "xdotool","click", "1", NULL };
-static const char *click2[]    = { "xdotool","click", "2", NULL };
-static const char *click3[]    = { "xdotool","click", "3", NULL };
-static const char *vol_up[]    = { "pamixer", "-u", "-i", "3", "--allow-boost", NULL };
-static const char *vol_down[]  = { "pamixer", "-u", "-d", "3", "--allow-boost", NULL };
-static const char *vol_mute[]  = { "amixer", "set", "Master", "mute", "-q", NULL };
-static const char *bright_up[]  = { "light", "-A", "5", NULL };
-static const char *bright_down[]  = { "light", "-U", "5", NULL };
+static const char *menucmd[]   = { "", NULL };
 ///--Custom foo---///
 static void halfandcentered(const Arg *arg)
 {
@@ -185,22 +175,11 @@ static key keys[] = {
     {  MOD |SHIFT,        XK_Right,      cursor_move,       {.i=TWOBWM_CURSOR_RIGHT}},
     {  MOD |SHIFT,        XK_Left,       cursor_move,       {.i=TWOBWM_CURSOR_LEFT}},
     // Start programs
-    {  MOD ,              XK_Return,     start,             {.com = terminal}},
     {  MOD ,              XK_w,          start,             {.com = menucmd}},
-    {  MOD |SHIFT,        XK_w,          start,             {.com = gmrun}},
     // Exit or restart 2bwm
     {  MOD |CONTROL,      XK_q,          twobwm_exit,       {.i=0}},
     {  MOD |CONTROL,      XK_r,          twobwm_restart,    {.i=0}},
     {  MOD ,              XK_space,      halfandcentered,   {.i=0}},
-    // Fake clicks using xdotool
-    {  MOD |CONTROL,      XK_Up,         start,             {.com = click1}},
-    {  MOD |CONTROL,      XK_Down,       start,             {.com = click2}},
-	{  MOD |CONTROL,      XK_Right,      start,             {.com = click3}},
-    {  0x000000,          0x1008ff13, start,             {.com = vol_up}},
-    {  0x000000,          0x1008ff11,  start,             {.com = vol_down}},
-    {  0x000000,          0x1008ff15, start,             {.com = vol_mute}},
-    {  0x000000,          0x1008ff02, start,             {.com = bright_up}},
-    {  0x000000,          0x1008ff03,  start,             {.com = bright_down}},
     // Change current workspace
        DESKTOPCHANGE(     XK_1,                             0)
        DESKTOPCHANGE(     XK_2,                             1)
