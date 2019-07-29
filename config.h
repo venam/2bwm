c9016b07705af54fbb9d7036a8513bb9e7a70d4a
Yang Li <git@netcrop.org>
2019-07-29 13:36:24 +0800

    memory leak fixed.

--- config.h
+++ config.h
@@ -40,7 +40,7 @@ static const uint8_t borders[] = {3,5,5,4};
 #define LOOK_INTO "WM_NAME"
 static const char *ignore_names[] = {"bar", "xclock"};
 ///--Menus and Programs---///
-static const char *menucmd[]   = { "", NULL };
+static const char *menucmd[]   = { "/bin/st", NULL };
 ///--Custom foo---///
 static void halfandcentered(const Arg *arg)
 {
